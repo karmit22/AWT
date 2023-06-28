b9a20d7914db491fc7e57c393c3be3334fe3e670
karmit22 <137374036+karmit22@users.noreply.github.com>
2023-06-28 15:31:37 +0530

    Add files via upload

---
+++ day8/javascript/demojsscript.js
@@ -0,0 +1,16 @@
+function ExternalAlert(){
+    alert('Hello Guys')
+}
+function ExternalAlert(){
+    if(confirm('are you sure..')){
+        alert('yess')
+    }
+    else{
+        alert('Noo')
+    }
+}
+function ExternalAlert(){
+    var fName=prompt('Enter First here')
+    var lName=prompt('Enter Lastname here')
+    alert(fName+" "+lName)
+}
